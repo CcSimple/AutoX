afd4f3a26f6852dee70a03b357ea8cba558d76ed
Cc-Mac <840054486@qq.com>
2021-04-03 02:57:38 +0800

    Compact code

--- inrt/src/main/java/com/stardust/auojs/inrt/SplashActivity.kt
+++ inrt/src/main/java/com/stardust/auojs/inrt/SplashActivity.kt
@@ -28,54 +28,21 @@ import java.util.*
 
 class SplashActivity : AppCompatActivity() {
     var TAG = "SplashActivity";
-    var step = 1; //打开悬浮权限，而打开权限，请求权限
-
     override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val slug = findViewById<TextView>(R.id.slug)
         slug.typeface = Typeface.createFromAsset(assets, "roboto_medium.ttf")
-        if (!Pref.isFirstUsing()) {//不是第一次
+        if (Pref.isFirstUsing()) {//不是第一次
             main()
         } else {
             Handler().postDelayed({ this@SplashActivity.main() }, INIT_TIMEOUT)
         }
     }
 
-
     private fun main() {
         checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_PHONE_STATE)
-    }
-
-    private fun manageDrawOverlays() {
-        var dialog = MaterialDialog.Builder(this).title("提示").content("请打开所有的权限，\r\n 省电策略选【不限制】")//内容
-                .positiveText("确定") //肯定按键
-                .onPositive { dialog, which ->
-                    SettingsCompat.manageDrawOverlays(this);
-                }.canceledOnTouchOutside(false)
-                .build();
-        dialog.show();
-    }
-
-    private fun manageWriteSettings() {
-        var dialog = MaterialDialog.Builder(this).title("继续进入权限设置").content("请打开所有权限!\r\n 请打开所有权限 \r\n 请打开所有权限")//内容
-                .positiveText("确定") //肯定按键
-                .onPositive { dialog, which ->
-                    IntentUtil.goToAppDetailSettings(this);
-                }.canceledOnTouchOutside(false)
-                .build();
-        dialog.show();
-    }
-
-    private fun AccessibilitySetting() {
-        var dialog = MaterialDialog.Builder(this).title("提示").content("请打开无障碍服务")//内容
-                .positiveText("确定") //肯定按键
-                .onPositive { dialog, which ->
-                    IntentUtils.gotoAccessibilitySetting();
-                }.canceledOnTouchOutside(false)
-                .build();
-        dialog.show();
     }
 
     private fun runScript() {
@@ -112,7 +79,6 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
-
     @RequiresApi(api = Build.VERSION_CODES.M)
     private fun getRequestPermissions(permissions: Array<out String>): Array<String> {
         val list = ArrayList<String>()
@@ -129,24 +95,5 @@ class SplashActivity : AppCompatActivity() {
         private const val INIT_TIMEOUT: Long = 2500
     }
 
-    override fun onResume() {
-        Log.d(TAG, "onResume: ")
-        if (!Pref.isFirstUsing()) { //已经不是第一次了
-            if (step == 1) {
-                manageDrawOverlays();
-            }
-            if (step == 2) {
-                manageWriteSettings();
-            }
-            if (step == 3) {
-                AccessibilitySetting();
-            }
-            if (step == 4) {
-                main();
-            }
-            step++;
-        }
-        super.onResume()
-    }
 }
 
